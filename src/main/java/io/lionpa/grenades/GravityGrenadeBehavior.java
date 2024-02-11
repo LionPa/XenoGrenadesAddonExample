3ddcb472d97602ae42b27bc567a2760b8be091bd
LionPa <LionPa2009@gmail.com>
2024-02-12 06:47:06 +0700

    ddvvdvdvd

--- src/main/java/io/lionpa/grenades/GravityGrenadeBehavior.java
+++ src/main/java/io/lionpa/grenades/GravityGrenadeBehavior.java
@@ -1,13 +1,21 @@
 package io.lionpa.grenades;
 
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.projectiles.ProjectileSource;
 
 public class GravityGrenadeBehavior implements GrenadeBehavior{
     @Override
     public void explode(Location location, Entity entity, ProjectileSource projectileSource) {
-
+        location.getNearbyEntities(2,1,2).forEach(entity1 -> {
+            if (entity1 instanceof LivingEntity living)
+                living.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION,20*5,1,false,false,false));
+        });
+        location.getWorld().spawnParticle(Particle.CLOUD,location,50,1,0.1,1,0.01f);
     }
 
     @Override
