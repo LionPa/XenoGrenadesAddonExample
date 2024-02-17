87db452e1362e7f9a40911159851dc2d4935ee8a
LionPa <LionPa2009@gmail.com>
2024-02-17 18:58:01 +0700

    fvfv

--- src/main/java/io/lionpa/grenades/GravityGrenadeBehavior.java
+++ src/main/java/io/lionpa/grenades/GravityGrenadeBehavior.java
@@ -3,11 +3,14 @@ package io.lionpa.grenades;
 import io.lionpa.Grenade;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.projectiles.ProjectileSource;
+import org.bukkit.util.Vector;
 
 public class GravityGrenadeBehavior implements GrenadeBehavior{
     @Override
@@ -20,6 +23,12 @@ public class GravityGrenadeBehavior implements GrenadeBehavior{
     }
     @Override
     public void fly(Location location, Entity entity) {}
+
+    @Override
+    public void collide(Location location, Entity entity, Block block, BlockFace blockFace, Vector vector) {
+
+    }
+
     @Override
     public void init(Grenade grenade) {
         // Add you own options
